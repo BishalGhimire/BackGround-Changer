aef11d4b325c12c3414272ab578277b7f3c5ab94
Bishal Ghimire <bishalghimire@Bishals-MacBook-Air.local>
2019-10-17 11:52:52 -0500

    button design

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
+export default class App extends React.Component {
+
+  myButtonPressed(){
+    Alert.alert("Back Ground chnaged");
+
+  }
+
+  render(){
+    return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity
+      onPress={this.myButtonPressed}>
+      <View style = {styles.text}>
+      <Text style ={{color: "#ffff"}}>Click Me</Text>
+      </View>
+      </TouchableOpacity>
+      
     </View>
   );
-}
+}}
 
 const styles = StyleSheet.create({
   container: {
@@ -15,5 +28,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    borderRadius: 10
+
   },
+  text: {
+    fontSize: 30,
+    backgroundColor: "#BB2CD9",
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderRadius: 15
+  },
+
 });
